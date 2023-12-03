3b1234b5a824d9073c5ddb97b628a92fb2cbbd7a
rubythulhu <95746090+rubythulhu@users.noreply.github.com>
2023-12-02 23:33:52 -0800

    fix gitignore and use another block for scoping

--- 02_nim/src/nimaoc02.nim
+++ 02_nim/src/nimaoc02.nim
@@ -1,5 +1,5 @@
 import std/[os, strutils, strformat]
-
+# have i gone too template-heavy? i don't care.
 
 type Counts = object
   r, g, b: int
@@ -11,19 +11,20 @@ template extractGame(line: string): tuple[id: int, samplestr: string] =
   let gparts = parts[0].split(' ')
   (gparts[1].parseInt, parts[1])
 
-template extractSamples(s: string): seq[Counts] = block:
-  var allCounts = newSeq[Counts]()
-  for sample in s.split("; "):
-    var counts: Counts 
-    for cubeset in sample.split(", "):
-      let parts = cubeset.split(" ");
-      case parts[1]:
-        of "red": counts.r = parts[0].parseInt
-        of "green": counts.g = parts[0].parseInt
-        of "blue": counts.b = parts[0].parseInt
-        else: discard
-    allCounts.add counts 
-  allCounts
+template extractSamples(s: string): seq[Counts] = 
+  block:
+    var allCounts = newSeq[Counts]()
+    for sample in s.split("; "):
+      var counts: Counts 
+      for cubeset in sample.split(", "):
+        let parts = cubeset.split(" ");
+        case parts[1]:
+          of "red": counts.r = parts[0].parseInt
+          of "green": counts.g = parts[0].parseInt
+          of "blue": counts.b = parts[0].parseInt
+          else: discard
+      allCounts.add counts 
+    allCounts
 
 template adjust(color: var int, val: int) = color = color.max val
 
@@ -32,24 +33,26 @@ template adjustAll(cur, val: Counts) =
   cur.g.adjust val.g
   cur.b.adjust val.b
 
-template parseline (line: string): tuple[id: int, c: Counts] = block:
-  let 
-    (id, samplestr) = extractGame line
-    samples = extractSamples samplestr
-    fst = samples[0]
+template parseline (line: string): tuple[id: int, c: Counts] = 
+  block:
+    let 
+      (id, samplestr) = extractGame line
+      samples = extractSamples samplestr
+      fst = samples[0]
 
-  var c: Counts = fst
+    var c: Counts = fst
 
-  for sample in samples[1..high samples]:
-    adjustAll(c, sample)
+    for sample in samples[1..high samples]:
+      adjustAll(c, sample)
 
-  (id, c)
+    (id, c)
 
-template linescore (line: string): tuple[p1, p2: int] = block: 
-  let (id, c) = parseline line
-  let p1 = if c.r > lim.r or c.g > lim.g or c.b > lim.b: 0 else: id
-  let p2 = c.r * c.g * c.b
-  (p1, p2)
+template linescore (line: string): tuple[p1, p2: int] = 
+  block: 
+    let (id, c) = parseline line
+    let p1 = if c.r > lim.r or c.g > lim.g or c.b > lim.b: 0 else: id
+    let p2 = c.r * c.g * c.b
+    (p1, p2)
 
 template parse*(fn: string): tuple[p1, p2: int] = 
   var s1, s2 = 0
