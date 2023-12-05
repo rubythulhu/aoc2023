335f7f8582dcf867b39ffeaf32a279529a59d821
rubythulhu <95746090+rubythulhu@users.noreply.github.com>
2023-12-05 06:48:29 -0800

    majority of nimpretty changes are whitespace @ eol, not bad

--- nim_aoc_2023/src/nim_aoc_2023/day04.nim
+++ nim_aoc_2023/src/nim_aoc_2023/day04.nim
@@ -1,15 +1,15 @@
-import std/[intsets , os , math , sequtils , strutils , strformat]
+import std/[intsets, os, math, sequtils, strutils, strformat]
 
-const 
+const
   sampleData = staticRead "../../inputs/04/sample.txt"
 
 type
-  Card = object 
+  Card = object
     num: int
     winning: IntSet
     yours: IntSet
 
-proc parseNums(nums: string): IntSet = 
+proc parseNums(nums: string): IntSet =
   nums
     .split(Whitespace)
     .filterIt(it.len > 0)
@@ -18,30 +18,30 @@ proc parseNums(nums: string): IntSet =
 
 proc results(card: Card): seq[int] =
   var s: seq[int]
-  for i in items(card.winning * card.yours): 
-    s.add i 
+  for i in items(card.winning * card.yours):
+    s.add i
   s
 
-proc score(card: Card): int = 
+proc score(card: Card): int =
   var x = 0
   for i in card.results:
     x = nextPowerOfTwo x + 1
   x
 
-proc day04(input: string): int = 
+proc day04(input: string): int =
   var totalScore = 0
   var cards = newSeq[Card]()
   let lines = input.split(Newlines)
   for line in lines.filterIt(it.len > 0):
     var card = Card(num: 0, winning: initIntSet(), yours: initIntSet())
-    let parts = line.split {':','|'}
+    let parts = line.split {':', '|'}
     card.num = parts[0].split(Whitespace)[^1].parseInt
     card.winning = parts[1].parseNums
-    card.yours = parts[2].parseNums 
+    card.yours = parts[2].parseNums
     totalScore.inc card.score
   totalScore
-  
-when isMainModule: 
+
+when isMainModule:
   block:
     let p1score = day04 sampleData
     echo "sample/day04: {p1score=}".fmt
