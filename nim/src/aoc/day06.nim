d812f7075498f8e914263beb681f1d488ee259f6
rubythulhu <95746090+rubythulhu@users.noreply.github.com>
2023-12-08 14:43:35 -0800

    sample response is good

--- nim/src/aoc/day06.nim
+++ nim/src/aoc/day06.nim
@@ -15,8 +15,29 @@ type
 proc `$`(races: Races): string =
   "Races(\n" & races.mapIt("    " & $it).join("\n") & "\n)"
 
-proc extractNums(input: string): seq[int] =
-  input.split(Whitespace).filterIt(it.len > 0).mapIt(parseInt $it)
+proc extractNums(input: string): seq[int] {.inline.} =
+  input
+    .split(Whitespace)
+    .filterIt(it.len > 0)
+    .mapIt(parseInt it)
+
+proc parseLine(line: string): seq[int] {.inline.} =
+  line.split(':')[1].extractNums
+
+proc winningStrategies(race: Race): int =
+  var left, right: int
+  for t in 1 .. race.time:
+    if t * (race.time - t) > race.distance:
+      left = t
+      break
+  for t in countdown(race.time, max(left, 1)):
+    if t * (race.time - t) > race.distance:
+      right = t
+      break
+  right + 1 - left
+
+proc winningStrategies(races: Races): int =
+  races.map(winningStrategies).foldl(a * b)
 
 proc initRaces(input: string): Races =
   var races: Races
@@ -27,8 +48,8 @@ proc initRaces(input: string): Races =
 
   for line in input.splitLines():
     if line.len == 0: continue
-    if line[0..4] == "Time:": times = line.split(":")[1].extractNums
-    elif line[0..8] == "Distance:": distances = line.split(":")[1].extractNums
+    if line[0..4] == "Time:": times = line.parseLine
+    elif line[0..8] == "Distance:": distances = line.parseLine
     else: raise LibraryError.newException "what: " & line
 
   if times.len != distances.len:
@@ -38,16 +59,14 @@ proc initRaces(input: string): Races =
     let distance = distances[idx]
     races.add (time, distance)
 
-
   races
 
 when isMainModule:
-  block:
-    let races = initRaces sampleData
-    echo $races
+  let races = initRaces(sampleData)
+  assert races == @[(7, 9), (15, 40), (30, 200)]
+  assert races.map(winningStrategies) == @[4, 8, 9]
+  assert races.winningStrategies == 288
 
-    # static testing of sampleData here
-    discard
-
+when isMainModule:
   let params = commandLineParams()
   if params.len != 1: quit("give me a file name", 1)
