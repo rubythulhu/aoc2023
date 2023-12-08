de6a53c3052881859b250d4b737ca551b3a634b9
rubythulhu <95746090+rubythulhu@users.noreply.github.com>
2023-12-08 15:02:32 -0800

    day6 part2 done

--- nim/aoc.nimble
+++ nim/aoc.nimble
@@ -1,22 +1,23 @@
 # Package
 
-version       = "0.1.0"
-author        = "rubythulhu"
-description   = "A new awesome nimble package"
-license       = "MIT"
-srcDir        = "src"
-installExt    = @["nim"]
-binDir        = "bin"
+version = "0.1.0"
+author = "rubythulhu"
+description = "A new awesome nimble package"
+license = "MIT"
+srcDir = "src"
+installExt = @["nim"]
+binDir = "bin"
 
 namedBin["aoc/day01"] = "day01"
 namedBin["aoc/day02"] = "day02"
 namedBin["aoc/day03"] = "day03"
 namedBin["aoc/day04"] = "day04"
 namedBin["aoc/day05"] = "day05"
+namedBin["aoc/day06"] = "day06"
 
 
 # Dependencies
- 
+
 requires "nim >= 2.0.0"
 
 task docs, "build docs for all":
--- nim/src/aoc/day06.nim
+++ nim/src/aoc/day06.nim
@@ -15,14 +15,24 @@ type
 proc `$`(races: Races): string =
   "Races(\n" & races.mapIt("    " & $it).join("\n") & "\n)"
 
-proc extractNums(input: string): seq[int] {.inline.} =
-  input
+proc extractNums(line: string): seq[int] {.inline.} =
+  line
     .split(Whitespace)
     .filterIt(it.len > 0)
     .mapIt(parseInt it)
 
 proc parseLine(line: string): seq[int] {.inline.} =
   line.split(':')[1].extractNums
+
+proc extractNumBadKerning(line: string): int {.inline.} =
+  line
+    .split(Whitespace)
+    .filterIt(it.len > 0)
+    .join("")
+    .parseInt
+
+proc parseLineBadKerning(line: string): int {.inline.} =
+  line.split(':')[1].extractNumBadKerning
 
 proc winningStrategies(race: Race): int =
   var left, right: int
@@ -61,12 +71,31 @@ proc initRaces(input: string): Races =
 
   races
 
+proc initRaceBadKerning(input: string): Race =
+  var time, distance: int
+  for line in input.splitLines():
+    if line.len == 0: continue
+    if line[0..4] == "Time:": time = line.parseLineBadKerning
+    elif line[0..8] == "Distance:": distance = line.parseLineBadKerning
+    else: raise LibraryError.newException "what: " & line
+  (time, distance)
+
 when isMainModule:
-  let races = initRaces(sampleData)
-  assert races == @[(7, 9), (15, 40), (30, 200)]
-  assert races.map(winningStrategies) == @[4, 8, 9]
-  assert races.winningStrategies == 288
+  block:
+    let races = initRaces(sampleData)
+    assert races == @[(7, 9), (15, 40), (30, 200)]
+    assert races.map(winningStrategies) == @[4, 8, 9]
+    assert races.winningStrategies == 288
+    let race = initRaceBadKerning(sampleData)
+    echo "Race: ", race
+    assert race == (71530, 940200)
+    assert race.winningStrategies == 71503
+
 
 when isMainModule:
   let params = commandLineParams()
   if params.len != 1: quit("give me a file name", 1)
+  let races = initRaces readFile params[0]
+  echo "winning strats: ", races.winningStrategies
+  let race = initRaceBadKerning readFile params[0]
+  echo "winning strats (kerning fixt): ", race.winningStrategies
