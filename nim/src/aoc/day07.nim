f834164348ed96f24e5f976ba6d0b76116ca8a7a
rubythulhu <95746090+rubythulhu@users.noreply.github.com>
2023-12-09 18:05:57 -0800

    day7part2 sampledata works

--- nim/src/aoc/day07.nim
+++ nim/src/aoc/day07.nim
@@ -4,6 +4,7 @@ import std/[
   sequtils,
   strformat,
   strutils,
+  sugar,
   tables,
 ]
 
@@ -11,25 +12,45 @@ const
   sampleData = staticRead "../../inputs/07/sample.txt"
 
 type
+
   Card = enum c2, c3, c4, c5, c6, c7, c8, c9, c10, cJ, cQ, cK, cA
+  WildCard = enum cJ, c2, c3, c4, c5, c6, c7, c8, c9, c10, cQ, cK, cA
   HandData = array[5, Card]
+  WildHandData = array[5, WildCard]
   HandType = enum HighCard, Pair, TwoPairs, ThreeOfAKind, FullHouse,
     FourOfAKind, FiveOfAKind
   HandComparison = enum Lose = -1, Tie = 0, Win = 1
   Hand = object
     data: HandData
+    wild: WildHandData
     handType: HandType
   Player = object
     hand: Hand
     bid: int
   Game = seq[Player]
 
+proc toWildCard(card: Card): WildCard =
+  case card
+  of cJ: result = cJ
+  of c2: result = c2
+  of c3: result = c3
+  of c4: result = c4
+  of c5: result = c5
+  of c6: result = c6
+  of c7: result = c7
+  of c8: result = c8
+  of c9: result = c9
+  of c10: result = c10
+  of cQ: result = cQ
+  of cK: result = cK
+  of cA: result = cA
+
 proc `$`(game: Game): string =
   "Game(\n" & game.mapIt("  " & $it).join("\n") & "\n)"
 
-proc handType(hand: HandData): HandType =
+proc handType[T](hand: openArray[T]): HandType =
   var
-    counts = initCountTable[Card]()
+    counts = initCountTable[T]()
     maxCount = 0
   for card in hand:
     counts.inc card
@@ -51,7 +72,7 @@ proc compareHandTypes(a, b: HandType): HandComparison =
   elif a > b: result = Win
   else: result = Lose
 
-proc compareSameHandType(a, b: HandData): HandComparison =
+proc compareSameHandType[T](a, b: openArray[T]): HandComparison =
   for idx, aCard in a:
     let bCard = b[idx]
     if aCard > bCard:
@@ -62,7 +83,7 @@ proc compareSameHandType(a, b: HandData): HandComparison =
       return
   result = Tie
 
-proc compareHandData(a, b: HandData): HandComparison =
+proc compareHandData[T](a, b: openArray[T]): HandComparison =
   var
     at = a.handType
     bt = b.handType
@@ -70,10 +91,14 @@ proc compareHandData(a, b: HandData): HandComparison =
   if at != bt: result = compareHandTypes(at, bt)
   else: result = compareSameHandType(a, b)
 
+
 proc sortPlayers(a, b: Player): int =
   compareHandData(a.hand.data, b.hand.data).ord
 
-proc charToCard(ch: char): Card {.inline.} =
+proc sortPlayersWild(a, b: Player): int =
+  compareHandData(a.hand.wild, b.hand.wild).ord
+
+proc toCard(ch: char): Card {.inline.} =
   case ch
   of '2': result = c2
   of '3': result = c3
@@ -90,7 +115,7 @@ proc charToCard(ch: char): Card {.inline.} =
   of 'A': result = cA
   else: quit "invalid card"
 
-proc cardToChar(card: Card): char {.inline.} =
+proc toChar(card: Card): char {.inline.} =
   case card
   of c2: result = '2'
   of c3: result = '3'
@@ -106,6 +131,26 @@ proc cardToChar(card: Card): char {.inline.} =
   of cK: result = 'K'
   of cA: result = 'A'
 
+proc toWildHandData(handData: HandData): WildHandData =
+  for i in 0..4:
+    result[i] = handData[i].toWildCard
+
+proc bestWildcardHand(handData: HandData): WildHandData =
+  let orig = handData.toWildHandData
+  result = orig
+  for i in WildCard.toSeq:
+    var alt = orig
+    for j in 0..4:
+      if alt[j] == cJ:
+        alt[j] = i
+    if compareHandData(alt, result) == Win:
+      result = alt
+
+proc gameResultsWild(game: Game): int =
+  result = 0
+  for i, player in game.sorted(sortPlayersWild):
+    result.inc (i + 1) * player.bid
+
 proc gameResults(game: Game): int =
   result = 0
   for i, player in game.sorted(sortPlayers):
@@ -115,9 +160,10 @@ proc gameResults(game: Game): int =
 proc initHand(handStr: string): Hand =
   var handData: array[0..4, Card]
   for i in 0..4:
-    handData[i] = handStr[i].charToCard
+    handData[i] = handStr[i].toCard
 
   result.data = handData
+  result.wild = handData.bestWildcardHand
   result.handType = handType result.data
 
 proc initGame(input: string): Game =
@@ -135,9 +181,13 @@ when isMainModule and not defined(release):
   block:
     let game = initGame sampleData
     assert game.gameResults == 6440
+    assert game.gameResultsWild == 5905
     # TODO: add sample data checks
     discard
 
 when isMainModule:
   let params = commandLineParams()
   if params.len != 1: quit("give me a file name", 1)
+  let game = initGame readFile params[0]
+  echo "day07/part1: {game.gameResults}".fmt
+  echo "day07/part2: {game.gameResultsWild}"
